cb2a4d72eb4126771e5163e6d2dcc333c1af1840
Fernando <fcano@elpuig.xeill.net>
2020-09-30 19:02:33 +0200

    --

--- app/src/main/java/com/example/multicontador/MainActivity.java
+++ app/src/main/java/com/example/multicontador/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.multicontador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int contador;
+    TextView contadordeclics;
+    Button augmentarElContador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
     }
 }
